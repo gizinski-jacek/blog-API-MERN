7ac32c6a5f91bdf894047b3cb5fba1c746f92713
mrjackmaster <jacektrg+github@gmail.com>
2022-01-19 16:19:03 +0100

    Initial file setup for MERN blog app

---
+++ server/app.js
@@ -0,0 +1,110 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const session = require('express-session');
+const compression = require('compression');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcryptjs = require('bcryptjs');
+const MongoStore = require('connect-mongo');
+const Member = require('./models/member');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const mongoose = require('mongoose');
+const mongoDb = process.env.MONGODB_URI;
+mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const app = express();
+
+passport.use(
+	new LocalStrategy((username, password, done) => {
+		Member.findOne({ username: username }, (err, user) => {
+			if (err) {
+				return done(err);
+			}
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username' });
+			}
+			comparePassword(password, user.password, (err, match) => {
+				if (err) {
+					return next(err);
+				}
+				if (match) {
+					return done(null, user);
+				} else {
+					return done(null, false, { message: 'Incorrect password' });
+				}
+			});
+		});
+	})
+);
+
+const comparePassword = (password, hash, cb) => {
+	bcryptjs.compare(password, hash, (err, equal) => {
+		if (err) {
+			return done(err);
+		}
+		cb(null, equal);
+	});
+};
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	Member.findById(id, (err, user) => {
+		done(err, user);
+	});
+});
+
+const sessionConfig = {
+	secret: process.env.PASSPORT_SECRET,
+	resave: false,
+	saveUninitialized: true,
+	store: MongoStore.create({ mongoUrl: mongoDb, collectionName: 'sessions' }),
+	cookie: { maxAge: 1000 * 60 * 60 * 24 }, // One day
+};
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+app.use(helmet());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
