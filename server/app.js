6649600216f577ff93722eff76eb79300839d6c6
mrjackmaster <jacektrg+github@gmail.com>
2022-01-21 18:56:48 +0100

    Cleanup, small fixes, readding pug for now to avoid getting errors, will fix this later. Moving passport to separate module.

--- server/app.js
+++ server/app.js
@@ -1,95 +1,62 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const helmet = require('helmet');
-const session = require('express-session');
 const compression = require('compression');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcryptjs = require('bcryptjs');
-const MongoStore = require('connect-mongo');
-const Member = require('./models/member');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 const mongoose = require('mongoose');
+
 const mongoDb = process.env.MONGODB_URI;
 mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
+
 const app = express();
 
-passport.use(
-	new LocalStrategy((username, password, done) => {
-		Member.findOne({ username: username }, (err, user) => {
-			if (err) {
-				return done(err);
-			}
-			if (!user) {
-				return done(null, false, { message: 'Incorrect username' });
-			}
-			comparePassword(password, user.password, (err, match) => {
-				if (err) {
-					return next(err);
-				}
-				if (match) {
-					return done(null, user);
-				} else {
-					return done(null, false, { message: 'Incorrect password' });
-				}
-			});
-		});
-	})
-);
+// const sessionConfig = {
+// 	secret: process.env.PASSPORT_SECRET,
+// 	resave: false,
+// 	saveUninitialized: true,
+// 	store: MongoStore.create({ mongoUrl: mongoDb, collectionName: 'sessions' }),
+// 	cookie: { maxAge: 1000 * 60 * 60 * 24 }, // One day
+// };
 
-const comparePassword = (password, hash, cb) => {
-	bcryptjs.compare(password, hash, (err, equal) => {
-		if (err) {
-			return done(err);
-		}
-		cb(null, equal);
-	});
-};
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-passport.serializeUser((user, done) => {
-	done(null, user.id);
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+	);
+	next();
 });
 
-passport.deserializeUser((id, done) => {
-	Member.findById(id, (err, user) => {
-		done(err, user);
-	});
-});
-
-const sessionConfig = {
-	secret: process.env.PASSPORT_SECRET,
-	resave: false,
-	saveUninitialized: true,
-	store: MongoStore.create({ mongoUrl: mongoDb, collectionName: 'sessions' }),
-	cookie: { maxAge: 1000 * 60 * 60 * 24 }, // One day
-};
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session(sessionConfig));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session(sessionConfig));
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -102,9 +69,9 @@ app.use(function (err, req, res, next) {
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
+	// respond with error
 	res.status(err.status || 500);
-	res.render('error');
+	res.json('404 not found');
 });
 
 module.exports = app;
