e700d6a9f73dfd00a94a1f721423ed866599de05
mrjackmaster <jacektrg+github@gmail.com>
2022-01-24 17:40:58 +0100

    Component displaying all posts

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,9 +1,11 @@
-const Posts = () => {
-	return (
-		<div>
-			<span>Posts</span>
-		</div>
-	);
+import PostWrapper from './utils/PostWrapper';
+
+const Posts = ({ posts }) => {
+	const postsDisplay = posts?.map((post) => {
+		return <PostWrapper key={post._id} post={post} />;
+	});
+
+	return <section className='posts-preview container'>{postsDisplay}</section>;
 };
 
 export default Posts;
