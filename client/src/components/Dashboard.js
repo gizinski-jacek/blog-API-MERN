26273bb55e361cf59ea40f2d376e6b3d96bcf708
mrjackmaster <jacektrg+github@gmail.com>
2022-01-24 17:40:08 +0100

    Dashboard component displaying logged users posts

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import UserPostWrapper from './utils/UserPostWrapper';
+
+const Dashboard = ({ user, posts }) => {
+	const postsDisplay = posts
+		?.filter((post) => post.author === user._id)
+		.map((post, index) => {
+			return <UserPostWrapper key={index} post={post} dashboard={true} />;
+		});
+
+	return <div className='user-posts container'>{postsDisplay}</div>;
+};
+
+export default Dashboard;
