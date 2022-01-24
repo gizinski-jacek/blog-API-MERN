9ca9209c2445f5f2717f4ce3a3a9b879e8d6cfed
mrjackmaster <jacektrg+github@gmail.com>
2022-01-24 17:40:21 +0100

    Wrapper for logged user posts

---
+++ client/src/components/utils/UserPostWrapper.js
@@ -0,0 +1,19 @@
+const PostWrapper = ({ post }) => {
+	return (
+		<div className='user-post'>
+			<div className='post-controls'>
+				<button type='button'>Edit</button>
+				<button type='button'>Delete</button>
+			</div>
+			<article className='post'>
+				<h2 className='title'>{post.title}</h2>
+				<h3 className='author'>
+					{post.author ? post.author.username : 'Anonymous'}
+				</h3>
+				<h3 className='published'>{post.timestamp}</h3>
+			</article>
+		</div>
+	);
+};
+
+export default PostWrapper;
