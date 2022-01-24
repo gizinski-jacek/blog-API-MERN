6a1ab35d02c67dd556c17bafbec6f9592483ac5f
mrjackmaster <jacektrg+github@gmail.com>
2022-01-24 17:46:34 +0100

    Component displaying limited number of posts on front page.

--- client/src/components/PostsPreview.js
+++ client/src/components/PostsPreview.js
@@ -1,9 +1,11 @@
-const PostsPreview = () => {
-	return (
-		<div>
-			<span>PostsPreview</span>
-		</div>
-	);
+import PostWrapper from './utils/PostWrapper';
+
+const PostsPreview = ({ posts }) => {
+	const postsDisplay = posts?.slice(0, 3).map((post) => {
+		return <PostWrapper key={post._id} post={post} />;
+	});
+
+	return <section className='posts-preview container'>{postsDisplay}</section>;
 };
 
 export default PostsPreview;
