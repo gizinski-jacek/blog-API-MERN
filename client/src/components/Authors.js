6164e0b6fa7f139a63d8410f23351840ec903a52
mrjackmaster <jacektrg+github@gmail.com>
2022-01-25 18:04:12 +0100

    Displaying list of authors together with their published posts

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -1,7 +1,31 @@
-const Authors = () => {
+import PostLinkWrapper from './utils/PostLinkWrapper';
+
+const Authors = (props) => {
+	// Grab author names from all posts
+	const authors = props.posts?.map((post) => {
+		return post.author;
+	});
+	// Remove duplicate author names
+	const uniqueAuthors = [...new Set(authors)];
+	// Transform author names to objects with author name and array of posts belonging to that author
+	const authorsData = uniqueAuthors.map((author) => {
+		let posts = props.posts.filter((post) => post.author === author);
+		return { author_name: author, posts: posts };
+	});
+	const contentDisplay = authorsData.map((author) => {
+		const posts = author.posts.map((post, index) => {
+			return <PostLinkWrapper key={index} post={post} />;
+		});
+		return (
+			<div className='author'>
+				<h1>{author.author_name}</h1>
+				{posts}
+			</div>
+		);
+	});
 	return (
-		<div className='authors'>
-			<span>Authors</span>
+		<div className='author-list'>
+			<span>{contentDisplay}</span>
 		</div>
 	);
 };
