74e1132dfb2771188e0540a1faa80051af337fb4
mrjackmaster <jacektrg+github@gmail.com>
2022-01-25 18:08:44 +0100

    Displaying single post details

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import PostDataWrapper from './utils/PostDataWrapper';
+
 const PostDetails = () => {
+	const params = useParams();
+
+	const [postData, setPostData] = useState();
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const res = await fetch(`/api/posts/${params.postid}`, {
+					method: 'GET',
+					mode: 'cors',
+					headers: { 'Content-type': 'application/json' },
+				});
+				const resJson = await res.json();
+				setPostData(resJson);
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, []);
+
 	return (
 		<div className='post-details'>
-			<span>PostDetails</span>
+			{postData ? <PostDataWrapper post={postData} /> : null}
 		</div>
 	);
 };
