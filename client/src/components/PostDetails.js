1c7fb4f221843df05fe936d4ac0694ffd7fc27dc
mrjackmaster <jacektrg+github@gmail.com>
2022-01-25 18:10:20 +0100

    Revert "Displaying single post details"
    This reverts commit 74e1132dfb2771188e0540a1faa80051af337fb4.

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,31 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import PostDataWrapper from './utils/PostDataWrapper';
-
 const PostDetails = () => {
-	const params = useParams();
-
-	const [postData, setPostData] = useState();
-
-	useEffect(() => {
-		(async () => {
-			try {
-				const res = await fetch(`/api/posts/${params.postid}`, {
-					method: 'GET',
-					mode: 'cors',
-					headers: { 'Content-type': 'application/json' },
-				});
-				const resJson = await res.json();
-				setPostData(resJson);
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
-
 	return (
 		<div className='post-details'>
-			{postData ? <PostDataWrapper post={postData} /> : null}
+			<span>PostDetails</span>
 		</div>
 	);
 };
