aef7211b247f9305c34f65bab9ee710e8d20c72c
mrjackmaster <jacektrg+github@gmail.com>
2022-01-25 18:02:06 +0100

    Cleanup, minor fixes and updating routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,6 +12,9 @@ router.get('/', function (req, res, next) {
 });
 
 /////
+/* GET logged in user */
+router.get('/authUser', user_controller.auth_user);
+
 /* POST log in */
 router.post('/log-in', user_controller.log_in_user);
 
@@ -19,36 +22,26 @@ router.post('/log-in', user_controller.log_in_user);
 router.post('/log-out', user_controller.log_out_user);
 
 /* POST sign up user */
-router.post('/sign-up', user_controller.sign_up_new_user);
-
-/* GET logged in user */
-router.get('/authUser', user_controller.check_user);
+router.post('/sign-up', user_controller.sign_up_user);
 
 /////
-/* POST new post */
-router.post('/create', verifyToken, post_controller.create_post);
-
-/* PUT user's post */
-router.get('/dashboard/:postid', post_controller.update_post);
-
-/* PUT update a post */
-router.put('/dashboard/:postid', post_controller.update_post);
-
-/* DELETE delete a post */
-router.delete('/dashboard/:postid', post_controller.delete_post);
-
-/////
-/* GET all users */
-router.get('/authors', user_controller.get_all_users);
-
-/* GET one user */
-router.get('/authors/:userid', user_controller.get_user);
-
 /* PUT update a user */
-router.put('/authors/:userid', user_controller.update_user);
+router.put('/dashboard', verifyToken, user_controller.update_user);
 
 /* DELETE delete a user */
-router.delete('/authors/:userid', user_controller.delete_user);
+router.delete('/dashboard', verifyToken, user_controller.delete_user);
+
+/* POST new post */
+router.post('/dashboard/create', verifyToken, post_controller.create_post);
+
+/* GET user's post */
+router.get('/dashboard/:postid', verifyToken, post_controller.update_post);
+
+/* PUT update a post */
+router.put('/dashboard/:postid', verifyToken, post_controller.update_post);
+
+/* DELETE delete a post */
+router.delete('/dashboard/:postid', verifyToken, post_controller.delete_post);
 
 /////
 /* GET all posts */
@@ -61,22 +54,22 @@ router.get('/posts/:postid', post_controller.get_post);
 /* GET all post's comments */
 router.get('/posts/:postid/comments', comment_controller.get_all_comments);
 
-/* GET one post's comment */
+/* GET one comment */
 router.get(
 	'/posts/:postid/comments/:commentid',
 	comment_controller.get_comment
 );
 
-/* POST new post's comment */
+/* POST new comment */
 router.post('/posts/:postid/comments', comment_controller.create_comment);
 
-/* PUT update post's comment */
+/* PUT update comment */
 router.put(
 	'/posts/:postid/comments/:commentid',
 	comment_controller.update_comment
 );
 
-/* DELETE delete post's comment */
+/* DELETE delete comment */
 router.delete(
 	'/posts/:postid/comments/:commentid',
 	comment_controller.delete_comment
