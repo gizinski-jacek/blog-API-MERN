082059a21d9f1d60dfc87fc7db343ae0e3e33de0
mrjackmaster <jacektrg+github@gmail.com>
2022-01-26 15:23:28 +0100

    Adding key referencing post

--- server/models/comment.js
+++ server/models/comment.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
 	text: { type: String, minlength: 2, maxlength: 64, required: true },
 	author: { type: String, required: true },
+	post_ref_id: { type: String, required: true },
 	timestamp: { type: Date, required: true },
 });
 
