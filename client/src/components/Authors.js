9b092b0709fdec28d1612c58a08a179f88ab6260
mrjackmaster <jacektrg+github@gmail.com>
2022-01-26 18:42:23 +0100

    Destructuring, cleanup.

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -1,16 +1,16 @@
 import PostLinkWrapper from './utils/PostLinkWrapper';
 
-const Authors = (props) => {
+const Authors = ({ posts }) => {
 	// Grab author names from all posts
-	const authors = props.posts?.map((post) => {
+	const authors = posts?.map((post) => {
 		return post.author;
 	});
 	// Remove duplicate author names
 	const uniqueAuthors = [...new Set(authors)];
 	// Transform author names to objects with author name and array of posts belonging to that author
 	const authorsData = uniqueAuthors.map((author) => {
-		let posts = props.posts.filter((post) => post.author === author);
-		return { author_name: author, posts: posts };
+		let authorPosts = posts.filter((post) => post.author === author);
+		return { author_name: author, authorPosts };
 	});
 	const contentDisplay = authorsData.map((author) => {
 		const posts = author.posts.map((post, index) => {
