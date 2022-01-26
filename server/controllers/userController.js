342764f4c36c71f98267dc50a8d0e8f6d6d68a19
mrjackmaster <jacektrg+github@gmail.com>
2022-01-26 18:50:09 +0100

    Rearranging. Minor changes (err => error) to stay consistent. Cleanup.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,10 +9,33 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const bcryptjs = require('bcryptjs');
 
+exports.auth_user = async (req, res, next) => {
+	try {
+		let currentUser;
+		if (req.cookies.userToken) {
+			const decoded = await jwt.verify(
+				req.cookies.userToken,
+				process.env.STRATEGY_SECRET
+			);
+			const user = await User.findById(decoded._id);
+			currentUser = {
+				_id: user._id,
+				username: user.username,
+				token: req.cookies.userToken,
+			};
+		} else {
+			currentUser = null;
+		}
+		res.status(200).json({ currentUser });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 exports.log_in_user = (req, res, next) => {
-	passport.authenticate('login', { session: false }, (err, user) => {
-		if (err) {
-			return next(err);
+	passport.authenticate('login', { session: false }, (error, user) => {
+		if (error) {
+			return next(error);
 		}
 		if (!user) {
 			return res.status(400).json([
@@ -25,13 +48,11 @@ exports.log_in_user = (req, res, next) => {
 			{ _id: user._id, username: user.username },
 			process.env.STRATEGY_SECRET,
 			{ expiresIn: '30m' },
-			(err, token) => {
-				if (err) {
-					return next(err);
+			(error, token) => {
+				if (error) {
+					return next(error);
 				}
-				/// DO LATER: lower maxAge.
 				res.cookie('userToken', token, {
-					maxAge: 600000999,
 					httpOnly: true,
 					secure: false,
 					sameSite: 'lax',
@@ -49,7 +70,6 @@ exports.log_out_user = (req, res, next) => {
 		secure: false,
 		sameSite: 'lax',
 	});
-
 	res.status(200).json({ success: true });
 };
 
@@ -75,9 +95,9 @@ exports.sign_up_user = [
 		const newUser = new User({
 			username: req.body.username,
 		});
-		User.find({ username: req.body.username }).exec((err, user_list) => {
-			if (err) {
-				return next(err);
+		User.find({ username: req.body.username }).exec((error, user_list) => {
+			if (error) {
+				return next(error);
 			}
 			if (user_list.length > 0) {
 				// Move this into custom body validator?
@@ -86,14 +106,14 @@ exports.sign_up_user = [
 			if (!errors.isEmpty()) {
 				return res.status(400).json(errors.array());
 			}
-			bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
-				if (err) {
-					return next(err);
+			bcryptjs.hash(req.body.password, 10, (error, hashedPassword) => {
+				if (error) {
+					return next(error);
 				}
 				newUser.password = hashedPassword;
-				newUser.save(newUser, (err) => {
-					if (err) {
-						return next(err);
+				newUser.save(newUser, (error) => {
+					if (error) {
+						return next(error);
 					}
 					return res.status(200).json({ success: true });
 				});
@@ -101,36 +121,3 @@ exports.sign_up_user = [
 		});
 	},
 ];
-
-exports.auth_user = async (req, res, next) => {
-	let currentUser;
-	if (req.cookies.userToken) {
-		const decoded = await jwt.verify(
-			req.cookies.userToken,
-			process.env.STRATEGY_SECRET
-		);
-		const user = await User.findById(decoded._id);
-		currentUser = {
-			_id: user._id,
-			username: user.username,
-			token: req.cookies.userToken,
-		};
-	} else {
-		currentUser = null;
-	}
-	res.status(200).json({ currentUser });
-};
-
-exports.update_user = (req, res, next) => {
-	if (!req.isAuthenticated()) {
-		return res.render('log-in', { title: 'Log In' });
-	}
-	/// Update user in db
-};
-
-exports.delete_user = (req, res, next) => {
-	if (!req.isAuthenticated()) {
-		return res.render('log-in', { title: 'Log In' });
-	}
-	/// Delete user from db
-};
