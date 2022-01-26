2a43e170fd2653acc768b0caaa8855e856b6e81e
mrjackmaster <jacektrg+github@gmail.com>
2022-01-26 18:47:06 +0100

    Fetching comments for specific post and rendering them

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
 const Comments = () => {
-	return (
-		<div className='comments'>
-			<span>Comments</span>
-		</div>
-	);
+	const params = useParams();
+
+	const [postComments, setPostComments] = useState();
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const res = await fetch(`/api/posts/${params.postid}/comments`, {
+					method: 'GET',
+					mode: 'cors',
+					headers: { 'Content-type': 'application/json' },
+				});
+				const resJson = await res.json();
+				setPostComments(resJson);
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, [params.postid]);
+
+	const commentsDisplay = postComments?.map((comment) => {
+		return (
+			<div className='comment'>
+				<h3>{comment.author}</h3>
+				<h3>{comment.timestamp}</h3>
+				<p>{comment.text}</p>
+			</div>
+		);
+	});
+
+	return <div className='comments'>{commentsDisplay}</div>;
 };
 
 export default Comments;
