1a9535fb67117d7e87a671a0f7c22a628cd9daac
mrjackmaster <jacektrg+github@gmail.com>
2022-01-27 00:59:42 +0100

    Single comment render component

--- client/src/components/CommentDetails.js
+++ client/src/components/CommentDetails.js
@@ -1,7 +1,18 @@
-const CommentDetails = () => {
+import { useState } from 'react';
+import { useParams } from 'react-router';
+
+const CommentDetails = ({ comments }) => {
+	const params = useParams();
+
+	const theComment = comments?.find((post) => post._id === params.postid);
+
 	return (
 		<div className='comment-details'>
-			<span>CommentDetails</span>
+			<div className='comment'>
+				<h3>{theComment.author}</h3>
+				<h3>{theComment.timestamp}</h3>
+				<p>{theComment.text}</p>
+			</div>
 		</div>
 	);
 };
