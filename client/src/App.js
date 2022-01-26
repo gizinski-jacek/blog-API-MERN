5118b878ea180a414de9230d9a1a316af8047368
mrjackmaster <jacektrg+github@gmail.com>
2022-01-26 18:42:09 +0100

    Creating routes for updating and deleting post. Adding 'loading' state to prevent rendering before data gets fetched.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import { Navigate } from 'react-router';
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -8,6 +9,7 @@ import PostsPreview from './components/PostsPreview';
 import Posts from './components/Posts';
 import PostDetails from './components/PostDetails';
 import Comments from './components/Comments';
+import CommentForm from './components/CommentForm';
 import CommentDetails from './components/CommentDetails';
 import Authors from './components/Authors';
 import Dashboard from './components/Dashboard';
@@ -16,6 +18,7 @@ import LogIn from './components/LogIn';
 import Signup from './components/Signup';
 
 const App = () => {
+	const [loading, setLoading] = useState(true);
 	const [user, setUser] = useState(null);
 	const [posts, setPosts] = useState();
 
@@ -30,6 +33,7 @@ const App = () => {
 				});
 				const resJson = await res.json();
 				setUser(resJson.currentUser);
+				setLoading(false);
 			} catch (error) {
 				console.log(error);
 			}
@@ -76,6 +80,7 @@ const App = () => {
 						element={
 							<>
 								<PostDetails posts={posts} />
+								<CommentForm user={user} />
 								<Comments />
 							</>
 						}
@@ -85,14 +90,60 @@ const App = () => {
 					<Route path='authors' element={<Authors posts={posts} />} />
 					<Route
 						path='dashboard'
-						element={<Dashboard user={user} posts={posts} />}
+						element={
+							loading ? null : user ? (
+								<Dashboard user={user} posts={posts} />
+							) : (
+								<Navigate to='/log-in' />
+							)
+						}
 					/>
-					<Route path='create' element={<PostForm user={user} />} />
+					<Route
+						path='dashboard/create'
+						element={
+							loading ? null : user ? (
+								<PostForm user={user} />
+							) : (
+								<Navigate to='log-in' />
+							)
+						}
+					/>
+					<Route
+						path='dashboard/:postid/update'
+						element={
+							loading ? null : user ? (
+								<PostForm posts={posts} editing={true} />
+							) : (
+								<Navigate to='log-in' />
+							)
+						}
+					/>
+					<Route
+						path='dashboard/:postid/delete'
+						element={
+							loading ? null : user ? (
+								<PostDetails posts={posts} deleting={true} />
+							) : (
+								<Navigate to='log-in' />
+							)
+						}
+					/>
 					<Route
 						path='log-in'
-						element={<LogIn user={user} setUser={setUser} />}
+						element={
+							loading ? null : user ? (
+								<Navigate to='/dashboard' />
+							) : (
+								<LogIn setUser={setUser} />
+							)
+						}
 					/>
-					<Route path='sign-up' element={<Signup />} />
+					<Route
+						path='sign-up'
+						element={
+							loading ? null : user ? <Navigate to='/dashboard' /> : <Signup />
+						}
+					/>
 				</Route>
 			</Routes>
 		</BrowserRouter>
