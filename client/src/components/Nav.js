eef107375ed0feef04a5dc794d16daace5aee6e1
mrjackmaster <jacektrg+github@gmail.com>
2022-01-26 18:43:11 +0100

    Destructuring, cleanup.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,21 +1,19 @@
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
-const Nav = (props) => {
+const Nav = ({ user, setUser }) => {
 	const navigate = useNavigate();
 
 	const handleLogOut = async (e) => {
 		e.preventDefault();
 		try {
-			const res = await fetch('/api/log-out', {
+			await fetch('/api/log-out', {
 				method: 'POST',
 				mode: 'cors',
 				headers: { 'Content-type': 'application/json' },
 			});
-			const resJson = await res.json();
-			console.log(resJson);
-			props.setUser(undefined);
 			navigate('/');
+			setUser(null);
 		} catch (error) {
 			console.log(error);
 		}
@@ -29,10 +27,10 @@ const Nav = (props) => {
 				<Link to='authors'>Authors</Link>
 			</div>
 			<div className='nav-right'>
-				{props.user ? (
+				{user ? (
 					<>
 						<Link to='dashboard'>Dashboard</Link>
-						<Link to='create'>Create Post</Link>
+						<Link to='dashboard/create'>Create Post</Link>
 						<Link to='/' onClick={handleLogOut}>
 							Log Out
 						</Link>
