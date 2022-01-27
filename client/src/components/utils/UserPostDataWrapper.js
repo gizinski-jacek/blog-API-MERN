3265fb8db22b23fed223cd553f5cccf14bd72763
mrjackmaster <jacektrg+github@gmail.com>
2022-01-27 18:36:04 +0100

    Adding publish/unpublish functionality. Adding last updated timestamp.

--- client/src/components/utils/UserPostDataWrapper.js
+++ client/src/components/utils/UserPostDataWrapper.js
@@ -1,25 +1,70 @@
 import { Link } from 'react-router-dom';
 
-const UserPostDataWrapper = ({ post }) => {
-	const handlePublish = () => {};
+const UserPostDataWrapper = ({ post, setPosts }) => {
+	const handlePublish = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await fetch(`/api/dashboard/${post._id}/publish`, {
+				method: 'POST',
+				mode: 'cors',
+				credentials: 'include',
+				headers: { 'Content-type': 'application/json' },
+			});
+			const resJson = await res.json();
+			if (res.status !== 200) {
+				// setErrors(resJson);
+				console.log(resJson);
+			} else {
+				setPosts(resJson);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-	const handleUnpublish = () => {};
+	const handleUnpublish = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await fetch(`/api/dashboard/${post._id}/unpublish`, {
+				method: 'POST',
+				mode: 'cors',
+				credentials: 'include',
+				headers: { 'Content-type': 'application/json' },
+			});
+			const resJson = await res.json();
+			if (res.status !== 200) {
+				// setErrors(resJson);
+				console.log(resJson);
+			} else {
+				setPosts(resJson);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<div className='user-post'>
 			<div className='post-controls'>
 				<Link to={`${post._id}/update`}>Edit</Link>
 				{post.published ? (
-					<button type='button'>Unpublish</button>
+					<button type='button' onClick={handleUnpublish}>
+						Unpublish
+					</button>
 				) : (
-					<button type='button'>Publish</button>
+					<button type='button' onClick={handlePublish}>
+						Publish
+					</button>
 				)}
 				<Link to={`${post._id}/delete`}>Delete</Link>
 			</div>
 			<article className='post'>
 				<h2 className='title'>{post.title}</h2>
 				<h3 className='author'>{post.author ? post.author : 'Anonymous'}</h3>
-				<h3 className='published'>{post.timestamp}</h3>
+				<h3 className='published'>{post.create_timestamp}</h3>
+				{post.update_timestamp ? (
+					<h3 className='updated'>Last updated: {post.update_timestamp}</h3>
+				) : null}
 			</article>
 		</div>
 	);
