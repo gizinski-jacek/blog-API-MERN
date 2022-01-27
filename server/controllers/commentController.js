47803c3dc64accf77755e2f9974c2961b0baf02c
mrjackmaster <jacektrg+github@gmail.com>
2022-01-27 18:27:15 +0100

    Refactoring to use async and try/catch. Adding controllers for updating/deleting a comment. Cleanup.

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,91 +1,79 @@
-const User = require('../models/user');
-const Post = require('../models/post');
 const Comment = require('../models/comment');
-const async = require('async');
 const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const bcryptjs = require('bcryptjs');
 
-exports.get_all_comments = (req, res, next) => {
-	Comment.find({})
-		.sort({ timestamp: 'desc' })
-		.exec((error, comment_list) => {
-			if (error) {
-				return next(error);
-			}
-			res.status(202).json(comment_list);
-		});
+exports.get_all_comments = async (req, res, next) => {
+	try {
+		const comment_list = await Comment.find({}).sort({ timestamp: 'desc' });
+		res.status(202).json(comment_list);
+	} catch (error) {
+		next(error);
+	}
 };
-
-exports.get_all_comments_for_post = (req, res, next) => {
-	Comment.find({ post_ref_id: req.params.postid })
-		.sort({ timestamp: 'desc' })
-		.exec((error, comment_list) => {
-			if (error) {
-				return next(error);
-			}
-			res.status(202).json(comment_list);
-		});
-};
-
-// exports.get_comment = (req, res, next) => {
-// 	Comment.findById(req.params.commentid).exec((error, comment) => {
-// 		if (error) {
-// 			return next(error);
-// 		}
-// 		if (comment == null) {
-// 			let err = new Error('Comment not found');
-// 			err.status = 404;
-// 			return next(error);
-// 		}
-// 		res.status(202).json(comment);
-// 	});
-// };
 
 exports.create_comment = [
-	body('comment', 'Text field can not be empty')
+	body('commentValue', 'Text field can not be empty')
 		.trim()
 		.isLength({ min: 4, max: 64 })
 		.escape(),
-	(req, res, next) => {
-		const errors = validationResult(req);
-		const newComment = new Comment({
-			text: req.body.comment,
-			author: req.decodedUser.username,
-			post_ref_id: req.params.postid,
-			timestamp: new Date(),
-		});
-		if (!errors.isEmpty()) {
-			res.status(401).json(errors.array());
-		}
-		newComment.save((error) => {
-			if (error) {
-				return next(error);
+	async (req, res, next) => {
+		try {
+			const errors = validationResult(req);
+			const newComment = new Comment({
+				text: req.body.commentValue,
+				author: req.decodedUser.username,
+				post_ref_id: req.params.postid,
+				timestamp: new Date(),
+			});
+			if (!errors.isEmpty()) {
+				res.status(401).json(errors.array());
 			}
-			Comment.find({})
-				.sort({ timestamp: 'desc' })
-				.exec((error, comment_list) => {
-					if (error) {
-						return next(error);
-					}
-					res.status(200).json(comment_list);
-				});
-		});
+			await newComment.save();
+			const comment_list = await Comment.find({}).sort({ timestamp: 'desc' });
+			res.status(200).json(comment_list);
+		} catch (error) {
+			next(error);
+		}
 	},
 ];
 
-exports.update_comment = (req, res, next) => {
-	if (!req.isAuthenticated()) {
-		return res.render('log-in', { title: 'Log In' });
-	}
-	/// Update comment
-};
+exports.update_comment = [
+	body('commentValue', 'Text field can not be empty')
+		.trim()
+		.isLength({ min: 2, max: 64 })
+		.escape(),
+	async (req, res, next) => {
+		try {
+			const commentToUpdate = await Comment.findById(req.params.commentid);
+			if (commentToUpdate == null) {
+				return res.status(404).json('Comment not found');
+			}
+			const errors = validationResult(req);
+			const updatedComment = new Comment({
+				text: req.body.commentValue,
+				author: commentToUpdate.author,
+				post_ref_id: commentToUpdate.post_ref_id,
+				create_timestamp: commentToUpdate.create_timestamp,
+				update_timestamp: new Date(),
+			});
+			if (!errors.isEmpty()) {
+				return res.status(401).json(errors.array());
+			}
+			await Comment.findByIdAndUpdate(req.params.commentid, updatedComment);
+			const comment_list = await Comment.find({}).sort({ timestamp: 'desc' });
+			res.status(200).json(comment_list);
+		} catch (error) {
+			next(error);
+		}
+	},
+];
 
-exports.delete_comment = (req, res, next) => {
-	if (!req.isAuthenticated()) {
-		return res.render('log-in', { title: 'Log In' });
+exports.delete_comment = async (req, res, next) => {
+	try {
+		await Comment.findByIdAndDelete(req.params.commentid);
+		const comment_list = await Comment.find({}).sort({ timestamp: 'desc' });
+		res.status(200).json(comment_list);
+	} catch (error) {
+		next(error);
 	}
-	/// Delete comment
 };
