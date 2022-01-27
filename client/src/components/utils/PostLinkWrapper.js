ae59ff07cb47306bf3e602ff59523621805ac42b
mrjackmaster <jacektrg+github@gmail.com>
2022-01-27 18:35:27 +0100

    Adding last updated timestamp

--- client/src/components/utils/PostLinkWrapper.js
+++ client/src/components/utils/PostLinkWrapper.js
@@ -6,7 +6,10 @@ const PostLinkWrapper = ({ post }) => {
 			<article className='post'>
 				<h2 className='title'>{post.title}</h2>
 				<h3 className='author'>{post.author ? post.author : 'Anonymous'}</h3>
-				<h3 className='published'>{post.timestamp}</h3>
+				<h3 className='published'>{post.create_timestamp}</h3>
+				{post.update_timestamp ? (
+					<h3 className='updated'>Last updated: {post.update_timestamp}</h3>
+				) : null}
 			</article>
 		</Link>
 	);
