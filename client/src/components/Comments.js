3596a646c260f9e151f18d00e27b6c4feee15248
mrjackmaster <jacektrg+github@gmail.com>
2022-01-27 01:08:32 +0100

    Cleanup after moving fetching functionality to separate custom hook.

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,36 +1,19 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
-const Comments = () => {
+const Comments = ({ comments }) => {
 	const params = useParams();
 
-	const [postComments, setPostComments] = useState();
-
-	useEffect(() => {
-		(async () => {
-			try {
-				const res = await fetch(`/api/posts/${params.postid}/comments`, {
-					method: 'GET',
-					mode: 'cors',
-					headers: { 'Content-type': 'application/json' },
-				});
-				const resJson = await res.json();
-				setPostComments(resJson);
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, [params.postid]);
-
-	const commentsDisplay = postComments?.map((comment) => {
-		return (
-			<div className='comment'>
-				<h3>{comment.author}</h3>
-				<h3>{comment.timestamp}</h3>
-				<p>{comment.text}</p>
-			</div>
-		);
-	});
+	const commentsDisplay = comments
+		?.filter((comment) => comment.post_ref_id === params.postid)
+		.map((comment, index) => {
+			return (
+				<div key={index} className='comment'>
+					<h3>{comment.author}</h3>
+					<h3>{comment.timestamp}</h3>
+					<p>{comment.text}</p>
+				</div>
+			);
+		});
 
 	return <div className='comments'>{commentsDisplay}</div>;
 };
