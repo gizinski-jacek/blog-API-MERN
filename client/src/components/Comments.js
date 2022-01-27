54a9bcd9fa519539ef60b26609063a2532984fc2
mrjackmaster <jacektrg+github@gmail.com>
2022-01-27 18:34:43 +0100

    Adding Link to comment and last updated timestamp

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Comments = ({ comments }) => {
 	const params = useParams();
@@ -7,11 +8,16 @@ const Comments = ({ comments }) => {
 		?.filter((comment) => comment.post_ref_id === params.postid)
 		.map((comment, index) => {
 			return (
-				<div key={index} className='comment'>
-					<h3>{comment.author}</h3>
-					<h3>{comment.timestamp}</h3>
-					<p>{comment.text}</p>
-				</div>
+				<Link to={`comments/${comment._id}`}>
+					<div key={index} className='comment'>
+						<h3>{comment.author}</h3>
+						<h3>{comment.create_timestamp}</h3>
+						{comment.update_timestamp ? (
+							<h3>Last updated: {comment.update_timestamp}</h3>
+						) : null}
+						<p>{comment.text}</p>
+					</div>
+				</Link>
 			);
 		});
 
