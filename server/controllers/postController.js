65d7a8a2add32bac8cf5c5b7349271489155d239
mrjackmaster <jacektrg+github@gmail.com>
2022-01-27 18:25:10 +0100

    Refactoring to use async and try/catch. Adding controllers for updating/deleting/publishing/unpublishing a post.

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,80 +1,117 @@
 require('dotenv').config();
-const User = require('../models/user');
 const Post = require('../models/post');
-const Comment = require('../models/comment');
-const async = require('async');
 const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const bcryptjs = require('bcryptjs');
+
+exports.get_all_posts = async (req, res, next) => {
+	try {
+		const post_list = await Post.find({}).sort({ timestamp: 'desc' });
+		res.status(200).json(post_list);
+	} catch (error) {
+		next(error);
+	}
+};
 
 exports.create_post = [
-	body('title', 'Title field can not be empty')
+	body('titleValue', 'Title field can not be empty')
 		.trim()
 		.isLength({ min: 4, max: 64 })
 		.escape(),
-	body('text', 'Text field can not be empty')
+	body('textValue', 'Text field can not be empty')
 		.trim()
 		.isLength({ min: 4, max: 512 })
 		.escape(),
-	(req, res, next) => {
-		const errors = validationResult(req);
-		const newPost = new Post({
-			title: req.body.title,
-			text: req.body.text,
-			author: req.decodedUser.username,
-			timestamp: new Date(),
-		});
-		if (!errors.isEmpty()) {
-			res.status(401).json(errors.array());
-		}
-		newPost.save((error) => {
-			if (error) {
-				return next(error);
+	async (req, res, next) => {
+		try {
+			const errors = validationResult(req);
+			const newPost = new Post({
+				title: req.body.titleValue,
+				text: req.body.textValue,
+				author: req.decodedUser.username,
+				timestamp: new Date(),
+			});
+			if (!errors.isEmpty()) {
+				return res.status(401).json(errors.array());
 			}
-			Post.find({})
-				.sort({ timestamp: 'desc' })
-				.exec((error, post_list) => {
-					if (error) {
-						return next(error);
-					}
-					res.status(200).json(post_list);
-				});
-		});
+			await newPost.save();
+			const post_list = await Post.find({}).sort({ timestamp: 'desc' });
+			res.status(200).json(post_list);
+		} catch (error) {
+			next(error);
+		}
 	},
 ];
 
-exports.update_post = (req, res, next) => {
-	if (!req.isAuthenticated()) {
-		return res.render('log-in', { title: 'Log In' });
-	}
-	/// Update post
-};
-
-exports.delete_post = (req, res, next) => {
-	if (!req.isAuthenticated()) {
-		return res.render('log-in', { title: 'Log In' });
-	}
-	/// Delete post
-};
-
-exports.get_all_posts = (req, res, next) => {
-	Post.find({})
-		.sort({ timestamp: 'desc' })
-		.exec((error, post_list) => {
-			if (error) {
-				return next(error);
+exports.update_post = [
+	body('titleValue', 'Title field can not be empty')
+		.trim()
+		.isLength({ min: 4, max: 64 })
+		.escape(),
+	body('textValue', 'Text field can not be empty')
+		.trim()
+		.isLength({ min: 4, max: 512 })
+		.escape(),
+	async (req, res, next) => {
+		try {
+			const postToUpdate = await Post.findById(req.params.postid);
+			if (postToUpdate == null) {
+				return res.status(404).json('Post not found');
 			}
+			const errors = validationResult(req);
+			const updatedPost = new Post({
+				title: req.body.titleValue,
+				text: req.body.textValue,
+				author: postToUpdate.author,
+				create_timestamp: postToUpdate.create_timestamp,
+				update_timestamp: new Date(),
+				published: false,
+			});
+			if (!errors.isEmpty()) {
+				return res.status(401).json(errors.array());
+			}
+			await Post.findByIdAndUpdate(req.params.postid, updatedPost);
+			const post_list = await Post.find({}).sort({ timestamp: 'desc' });
 			res.status(200).json(post_list);
-		});
+		} catch (error) {
+			next(err);
+		}
+	},
+];
+
+exports.delete_post = async (req, res, next) => {
+	try {
+		await Post.findByIdAndDelete(req.params.postid);
+		const post_list = await Post.find({}).sort({ timestamp: 'desc' });
+		res.status(200).json(post_list);
+	} catch (error) {
+		next(err);
+	}
 };
 
-// exports.get_post = (req, res, next) => {
-// 	Post.findById(req.params.postid).exec((error, post) => {
-// 		if (error) {
-// 			return next(error);
-// 		}
-// 		res.status(200).json(post);
-// 	});
-// };
+exports.publish_post = async (req, res, next) => {
+	try {
+		const thePost = await Post.findById(req.params.postid);
+		if (thePost == null) {
+			return res.status(404).json('Post not found');
+		}
+		await Post.findByIdAndUpdate(req.params.postid, { published: true });
+		const post_list = await Post.find({}).sort({ timestamp: 'desc' });
+		res.status(200).json(post_list);
+	} catch (error) {
+		next(err);
+	}
+};
+
+exports.unpublish_post = async (req, res, next) => {
+	try {
+		const thePost = await Post.findById(req.params.postid);
+		if (thePost == null) {
+			return res.status(404).json('Post not found');
+		}
+		await Post.findByIdAndUpdate(req.params.postid, { published: false });
+		const post_list = await Post.find({}).sort({ timestamp: 'desc' });
+		res.status(200).json(post_list);
+	} catch (error) {
+		next(err);
+	}
+};
