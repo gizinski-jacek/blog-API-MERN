80af8dcb1ac0e91659d0dfa04bfaf562bb2bdc41
mrjackmaster <jacektrg+github@gmail.com>
2022-01-28 17:30:50 +0100

    Cleanup

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -4,13 +4,12 @@ const jwt = require('jsonwebtoken');
 const verifyToken = async (req, res, next) => {
 	try {
 		if (typeof req.cookies.userToken === undefined) {
-			res.status(403).json([{ msg: 'Unauthorized Access' }]);
+			res.status(403).json({ msg: 'Unauthorized Access' });
 		} else {
-			req.decodedUser = await jwt.verify(
+			req.decodedUser = jwt.verify(
 				req.cookies.userToken,
 				process.env.STRATEGY_SECRET
 			);
-			req.token = req.cookies.userToken;
 			next();
 		}
 	} catch (error) {
