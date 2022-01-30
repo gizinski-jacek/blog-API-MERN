4104cbe650d7882788e0d2afd01f096a4ca23250
mrjackmaster <jacektrg+github@gmail.com>
2022-01-30 19:55:39 +0100

    Renaming component

--- client/src/components/WelcomePanel.js
+++ client/src/components/WelcomePanel.js
@@ -3,7 +3,8 @@ const WelcomePanel = () => {
 		<div className='welcome-panel'>
 			<h1>Welcome to Blogster</h1>
 			<h2>
-				Read and share Your thoughts and ideas about any topic imaginable.
+				Read and share Your thoughts and ideas about any topic You can imagine.
+				This is your mind's playground.
 			</h2>
 		</div>
 	);
