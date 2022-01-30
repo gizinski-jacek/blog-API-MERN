4ddf60d1b1a46bac2ff90de1a138692229c5430a
mrjackmaster <jacektrg+github@gmail.com>
2022-01-30 23:08:34 +0100

    Cleanup

--- client/src/components/utils/PostDataWrapper.js
+++ client/src/components/utils/PostDataWrapper.js
@@ -1,12 +1,15 @@
 const PostDataWrapper = ({ post }) => {
 	return (
 		<article className='post'>
-			<h2 className='post-title'>{post.title}</h2>
+			<h2 className='post-title'>Title: {post.title}</h2>
 			<p className='post-text'>{post.text}</p>
 			<h3 className='post-author'>Author: {post.author}</h3>
 			<h3 className='post-created'>Published: {post.create_timestamp}</h3>
 			{post.update_timestamp ? (
-				<h3 className='post-updated'>Last updated: {post.update_timestamp}</h3>
+				<h3 className='post-updated'>
+					Last updated:{' '}
+					{new Date(post.update_timestamp).toLocaleString('en-GB')}
+				</h3>
 			) : null}
 		</article>
 	);
--- client/src/components/utils/PostLinkWrapper.js
+++ client/src/components/utils/PostLinkWrapper.js
@@ -2,17 +2,18 @@ import { Link } from 'react-router-dom';
 
 const PostLinkWrapper = ({ post }) => {
 	return (
-		<Link to={`/posts/${post._id}`}>
-			<article className='post'>
-				<h2 className='post-title'>{post.title}</h2>
-				<h3 className='post-author'>Author: {post.author}</h3>
-				<h3 className='post-created'>Published: {post.create_timestamp}</h3>
-				{post.update_timestamp ? (
-					<h3 className='post-updated'>
-						Last updated: {post.update_timestamp}
-					</h3>
-				) : null}
-			</article>
+		<Link to={`/posts/${post._id}`} className='post'>
+			<h2 className='post-title'>Title: {post.title}</h2>
+			<h3 className='post-author'>Author: {post.author}</h3>
+			<h3 className='post-created'>
+				Published: {new Date(post.create_timestamp).toLocaleString('en-GB')}
+			</h3>
+			{post.update_timestamp ? (
+				<h3 className='post-updated'>
+					Last updated:{' '}
+					{new Date(post.update_timestamp).toLocaleString('en-GB')}
+				</h3>
+			) : null}
 		</Link>
 	);
 };
