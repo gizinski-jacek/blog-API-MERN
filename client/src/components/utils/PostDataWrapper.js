57c0d4f7b7354f46138cacdb7e5267415589c6b8
mrjackmaster <jacektrg+github@gmail.com>
2022-01-31 18:16:02 +0100

    Updating

--- client/src/components/utils/PostDataWrapper.js
+++ client/src/components/utils/PostDataWrapper.js
@@ -3,7 +3,7 @@ const PostDataWrapper = ({ post }) => {
 		<article className='post'>
 			<h2 className='post-title'>Title: {post.title}</h2>
 			<p className='post-text'>{post.text}</p>
-			<h3 className='post-author'>Author: {post.author}</h3>
+			<h3 className='post-author'>Author: {post.author.username}</h3>
 			<h3 className='post-created'>Published: {post.create_timestamp}</h3>
 			{post.update_timestamp ? (
 				<h3 className='post-updated'>
--- client/src/components/utils/PostLinkWrapper.js
+++ client/src/components/utils/PostLinkWrapper.js
@@ -4,7 +4,7 @@ const PostLinkWrapper = ({ post }) => {
 	return (
 		<Link to={`/posts/${post._id}`} className='post'>
 			<h2 className='post-title'>Title: {post.title}</h2>
-			<h3 className='post-author'>Author: {post.author}</h3>
+			<h3 className='post-author'>Author: {post.author.username}</h3>
 			<h3 className='post-created'>
 				Published: {new Date(post.create_timestamp).toLocaleString('en-GB')}
 			</h3>
