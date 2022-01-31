0e13a0aac9be08f26e593a3c6f6cdb7737845cb9
mrjackmaster <jacektrg+github@gmail.com>
2022-01-31 18:23:49 +0100

    Adding second fetch for list of users/authors instead of doing objects transormations since authors are now objects instead of strings. Doing them sequentially to preserve order of responses. Cleanup and update.

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -3,51 +3,54 @@ import PostLinkWrapper from './utils/PostLinkWrapper';
 
 const Authors = () => {
 	const [allPosts, setAllPosts] = useState();
+	const [allAuthors, setAllAuthors] = useState();
 
 	useEffect(() => {
 		(async () => {
 			try {
-				const res = await fetch(`/api/posts`, {
+				const resPosts = await fetch(`/api/posts`, {
 					method: 'GET',
 					mode: 'cors',
 					headers: { 'Content-type': 'application/json' },
 				});
-				const resJson = await res.json();
-				setAllPosts(resJson);
+				const resAuthors = await fetch(`/api/authors`, {
+					method: 'GET',
+					mode: 'cors',
+					headers: { 'Content-type': 'application/json' },
+				});
+				const postsJson = await resPosts.json();
+				const authorsJson = await resAuthors.json();
+				setAllPosts(postsJson);
+				setAllAuthors(authorsJson);
 			} catch (error) {
 				console.log(error);
 			}
 		})();
 	}, []);
 
-	// Grab author names from all posts
-	const authors = allPosts?.map((post) => {
-		return post.author;
-	});
-	// Remove duplicate author names
-	const uniqueAuthors = [...new Set(authors)];
-	// Transform author names to objects with author name and array of published posts belonging to that author
-	const authorsData = uniqueAuthors
-		.map((author) => {
+	// Transform each author object to objects with author data and array of published posts belonging to that author
+	const authorsData = allAuthors
+		?.map((author) => {
 			const authorPosts = allPosts.filter(
-				(post) => post.published === true && post.author === author
+				(post) => post.published === true && post.author._id === author._id
 			);
-			return { author_name: author, authorPosts };
+			return { author, authorPosts };
 		})
 		// Filter out authors with no published posts
 		.filter((author) => author.authorPosts.length > 0);
 
-	const contentDisplay = authorsData.map((author, index) => {
-		const posts = author.authorPosts.map((post, index) => {
+	const contentDisplay = authorsData?.map((data, index) => {
+		const posts = data.authorPosts.map((post, index) => {
 			return <PostLinkWrapper key={index} post={post} />;
 		});
 		return (
 			<div key={index} className='author'>
-				<h1>{author.author_name}</h1>
+				<h1>{data.author.username}</h1>
 				{posts}
 			</div>
 		);
 	});
+
 	return (
 		<div className='author-list'>
 			<span>{contentDisplay}</span>
