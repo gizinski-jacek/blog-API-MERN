125763d18eeb57afaf5dcec235e7606d664063a2
mrjackmaster <jacektrg+github@gmail.com>
2022-01-31 18:13:13 +0100

    Changing author and post_ref_id to reference User schema

--- server/models/comment.js
+++ server/models/comment.js
@@ -4,8 +4,12 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
 	text: { type: String, minlength: 2, maxlength: 64, required: true },
-	author: { type: String, required: true },
-	post_ref_id: { type: String, required: true },
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
 	create_timestamp: { type: Date, required: true },
 	update_timestamp: { type: Date },
 });
