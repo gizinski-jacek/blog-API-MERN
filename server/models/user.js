7b6649478d2e2dc833946aeff7d4b8b9a595fe54
mrjackmaster <jacektrg+github@gmail.com>
2022-02-01 19:05:00 +0100

    Minor addition

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	username: { type: String, minlength: 4, maxlength: 32, required: true },
 	password: { type: String, minlength: 4, maxlength: 64, required: true },
+	registered: { type: Date, required: true },
 	admin: { type: Boolean, default: false },
 });
 
