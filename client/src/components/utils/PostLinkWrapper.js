0da81171513416324210756f685ad87e35d030ad
mrjackmaster <jacektrg+github@gmail.com>
2022-02-02 23:17:28 +0100

    Formatting date with less info. Adding post text, removing update timestamp

--- client/src/components/utils/PostLinkWrapper.js
+++ client/src/components/utils/PostLinkWrapper.js
@@ -4,16 +4,16 @@ const PostLinkWrapper = ({ post }) => {
 	return (
 		<Link to={`/posts/${post._id}`} className='post'>
 			<h2 className='post-title'>Title: {post.title}</h2>
+			<p className='post-text'>{post.text}</p>
 			<h3 className='post-author'>Author: {post.author.username}</h3>
 			<h3 className='post-created'>
-				Published: {new Date(post.create_timestamp).toLocaleString('en-GB')}
+				Published:{' '}
+				{new Date(post.create_timestamp).toLocaleString('en-GB', {
+					year: 'numeric',
+					month: 'numeric',
+					day: 'numeric',
+				})}
 			</h3>
-			{post.update_timestamp ? (
-				<h3 className='post-updated'>
-					Last updated:{' '}
-					{new Date(post.update_timestamp).toLocaleString('en-GB')}
-				</h3>
-			) : null}
 		</Link>
 	);
 };
