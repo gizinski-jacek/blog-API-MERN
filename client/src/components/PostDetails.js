6965c1bdb7245f91654088b4bc3788b05fedbb43
mrjackmaster <jacektrg+github@gmail.com>
2022-02-02 23:10:51 +0100

    Moving in functionality of PostDataWrapper

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
-import PostDataWrapper from './utils/PostDataWrapper';
 
 const PostDetails = ({ deleting }) => {
 	const navigate = useNavigate();
@@ -54,7 +53,22 @@ const PostDetails = ({ deleting }) => {
 					</button>
 				</div>
 			) : null}
-			{post ? <PostDataWrapper post={post} /> : null}
+			{post ? (
+				<article className='post'>
+					<h2 className='post-title'>Title: {post.title}</h2>
+					<p className='post-text'>{post.text}</p>
+					<h3 className='post-author'>Author: {post.author.username}</h3>
+					<h3 className='post-created'>
+						Published: {new Date(post.create_timestamp).toLocaleString('en-GB')}
+					</h3>
+					{post.update_timestamp ? (
+						<h3 className='post-updated'>
+							Last updated:{' '}
+							{new Date(post.update_timestamp).toLocaleString('en-GB')}
+						</h3>
+					) : null}
+				</article>
+			) : null}
 		</div>
 	);
 };
