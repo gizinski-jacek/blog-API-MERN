49c068ba2c113585816699eecadd9c32135095d8
mrjackmaster <jacektrg+github@gmail.com>
2022-02-02 23:14:15 +0100

    Implementing login functionality directly in navbar. Sending to log-in page if there are errors. Passing errors to said page with useNavigate state.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,8 +1,42 @@
-import { useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
 const Nav = ({ currentUser, setCurrentUser }) => {
 	const navigate = useNavigate();
+	const location = useLocation();
+
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [showForm, setShowForm] = useState(false);
+
+	useEffect(() => {
+		setShowForm(false);
+	}, [location]);
+
+	const handleLogIn = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await fetch('/api/log-in', {
+				method: 'POST',
+				mode: 'cors',
+				credentials: 'include',
+				body: JSON.stringify({ username, password }),
+				headers: { 'Content-type': 'application/json' },
+			});
+			const resJson = await res.json();
+			setShowForm(false);
+			setUsername('');
+			setPassword('');
+			if (res.status !== 200) {
+				navigate('/log-in', { state: resJson });
+			} else {
+				setCurrentUser(resJson);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	const handleLogOut = async (e) => {
 		e.preventDefault();
@@ -12,7 +46,6 @@ const Nav = ({ currentUser, setCurrentUser }) => {
 				mode: 'cors',
 				headers: { 'Content-type': 'application/json' },
 			});
-			navigate('/');
 			setCurrentUser(null);
 		} catch (error) {
 			console.log(error);
@@ -25,12 +58,8 @@ const Nav = ({ currentUser, setCurrentUser }) => {
 				<Link to='/' className='logo'>
 					Blogster
 				</Link>
-				<Link to='posts' className='button-link'>
-					Posts
-				</Link>
-				<Link to='authors' className='button-link'>
-					Authors
-				</Link>
+				<Link to='posts'>Posts</Link>
+				<Link to='authors'>Authors</Link>
 			</div>
 			<div className='nav-right'>
 				{currentUser ? (
@@ -41,15 +70,59 @@ const Nav = ({ currentUser, setCurrentUser }) => {
 						<Link to='dashboard/create' className='button-link'>
 							Create Post
 						</Link>
-						<Link to='/' className='button-link' onClick={handleLogOut}>
+						<Link to='' className='button-link' onClick={handleLogOut}>
 							Log Out
 						</Link>
 					</>
 				) : (
 					<>
-						<Link to='log-in' className='button-link'>
-							Log In
-						</Link>
+						{showForm ? (
+							<form id='nav-log-in-form' onSubmit={handleLogIn}>
+								<fieldset>
+									<label htmlFor='username'>Username</label>
+									<input
+										type='text'
+										id='username'
+										name='username'
+										value={username}
+										onChange={(e) => setUsername(e.target.value)}
+										required
+										placeholder='Username'
+									/>
+									<label htmlFor='password'>Password</label>
+									<input
+										type='password'
+										id='password'
+										name='password'
+										value={password}
+										onChange={(e) => setPassword(e.target.value)}
+										required
+										placeholder='Password'
+									/>
+								</fieldset>
+								<div className='nav-log-in-controls'>
+									<button type='submit'>Log In</button>
+									<button
+										type='button'
+										onClick={() => {
+											setShowForm(false);
+										}}
+									>
+										Cancel
+									</button>
+								</div>
+							</form>
+						) : (
+							<button
+								to=''
+								className='button-link'
+								onClick={() => {
+									setShowForm(true);
+								}}
+							>
+								Log In
+							</button>
+						)}
 						<Link to='sign-up' className='button-link'>
 							Sign Up
 						</Link>
