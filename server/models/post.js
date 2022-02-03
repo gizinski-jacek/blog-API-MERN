b91734e55bcc971ed81d2c4717021b5cefa03243
mrjackmaster <jacektrg+github@gmail.com>
2022-02-03 23:48:08 +0100

    Increasing length of post title and text contents

--- server/models/post.js
+++ server/models/post.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-	title: { type: String, minlength: 4, maxlength: 64, required: true },
-	text: { type: String, minlength: 4, maxlength: 512, required: true },
+	title: { type: String, minlength: 4, maxlength: 128, required: true },
+	text: { type: String, minlength: 4, maxlength: 1024, required: true },
 	author: {
 		type: Schema.Types.ObjectId,
 		ref: 'User',
