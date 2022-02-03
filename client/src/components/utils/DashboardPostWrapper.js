07b441c5dfb63b891bd1681ebb703300c9302e63
mrjackmaster <jacektrg+github@gmail.com>
2022-02-03 23:39:31 +0100

    Minor restructuring

--- client/src/components/utils/DashboardPostWrapper.js
+++ client/src/components/utils/DashboardPostWrapper.js
@@ -56,15 +56,17 @@ const DashboardPostWrapper = ({ post, setAllPosts }) => {
 			</div>
 			<article className='post'>
 				<h2 className='post-title'>Title: {post.title}</h2>
-				<h3 className='post-author'>Author: {post.author.username}</h3>
-				<h3 className='post-created'>
-					Published:{' '}
-					{new Date(post.create_timestamp).toLocaleString('en-GB', {
-						year: 'numeric',
-						month: 'numeric',
-						day: 'numeric',
-					})}
-				</h3>
+				<div className='post-metadata'>
+					<h3 className='post-author'>Author: {post.author.username}</h3>
+					<h3 className='post-created'>
+						Published:{' '}
+						{new Date(post.create_timestamp).toLocaleString('en-GB', {
+							year: 'numeric',
+							month: 'numeric',
+							day: 'numeric',
+						})}
+					</h3>
+				</div>
 			</article>
 		</div>
 	);
