4264712b2c2099c808f74472036a8f34ae3a9ee2
mrjackmaster <jacektrg+github@gmail.com>
2022-02-03 23:41:16 +0100

    Component wrapping each authors posts into expandable list

---
+++ client/src/components/utils/AuthorPostsWrapper.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const AuthorPostsWrapper = ({ author, posts }) => {
+	const [expand, setExpand] = useState(false);
+
+	const postsDisplay = posts.map((post, index) => {
+		return (
+			<Link key={index} to={`/posts/${post._id}`} className='post-simple'>
+				<h2 className='post-title'>Title: {post.title}</h2>
+				<h2 className='post-created'>
+					Published:{' '}
+					{new Date(post.create_timestamp).toLocaleString('en-GB', {
+						year: 'numeric',
+						month: 'numeric',
+						day: 'numeric',
+					})}
+				</h2>
+			</Link>
+		);
+	});
+
+	return (
+		<div className='author-posts'>
+			<div className='top'>
+				<h2>Author: {author.username}</h2>
+				<button type='button' onClick={() => setExpand(!expand)}>
+					{expand ? 'Collapse' : 'Expand'}
+				</button>
+			</div>
+			<section className={`container ${expand ? 'expand' : ''}`}>
+				{postsDisplay}
+			</section>
+		</div>
+	);
+};
+
+export default AuthorPostsWrapper;
