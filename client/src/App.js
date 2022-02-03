dc5cce1af8b9672d12ee0e937061b43111610dc8
mrjackmaster <jacektrg+github@gmail.com>
2022-02-03 23:37:22 +0100

    Moving BrowserRouter back to index.js. Same reason as commt adbaf49

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { Navigate } from 'react-router';
-import { Routes, Route, Outlet, BrowserRouter } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import WelcomePanel from './components/WelcomePanel';
@@ -17,8 +17,11 @@ import LogIn from './components/LogIn';
 import Signup from './components/Signup';
 
 const App = () => {
+	const location = useLocation();
+
 	const [loading, setLoading] = useState(true);
 	const [currentUser, setCurrentUser] = useState(null);
+	const [posts, setPosts] = useState();
 
 	useEffect(() => {
 		(async () => {
@@ -40,94 +43,89 @@ const App = () => {
 				console.log(error);
 			}
 		})();
-	}, []);
+	}, [location]);
 
 	return (
-		<BrowserRouter>
-			<Routes>
+		<Routes>
+			<Route
+				path='/'
+				element={
+					loading ? null : (
+						<main className='main'>
+							<Navbar
+								currentUser={currentUser}
+								setCurrentUser={setCurrentUser}
+							/>
+							<Outlet />
+							<Footer />
+						</main>
+					)
+				}
+			>
 				<Route
-					path='/'
+					path=''
 					element={
-						loading ? null : (
-							<main className='main'>
-								<Navbar
-									currentUser={currentUser}
-									setCurrentUser={setCurrentUser}
-								/>
+						<>
+							<WelcomePanel />
+							<PostsPreview />
+						</>
+					}
+				/>
+				<Route
+					path='log-in'
+					element={
+						currentUser ? (
+							<Navigate to='/dashboard' />
+						) : (
+							<LogIn setCurrentUser={setCurrentUser} />
+						)
+					}
+				/>
+				<Route
+					path='sign-up'
+					element={currentUser ? <Navigate to='/dashboard' /> : <Signup />}
+				/>
+				<Route
+					path='dashboard'
+					element={
+						currentUser ? (
+							<>
 								<Outlet />
-								<Footer />
-							</main>
+							</>
+						) : (
+							<Navigate to='/log-in' />
 						)
 					}
 				>
+					<Route path='' element={<Dashboard currentUser={currentUser} />} />
+					<Route path='create' element={<PostForm />} />
+					<Route path='update/:postid' element={<PostForm editing={true} />} />
 					<Route
-						path=''
-						element={
-							<>
-								<WelcomePanel />
-								<PostsPreview />
-							</>
-						}
+						path='delete/:postid'
+						element={<PostDetails deleting={true} />}
 					/>
-					<Route
-						path='log-in'
-						element={
-							currentUser ? (
-								<Navigate to='/dashboard' />
-							) : (
-								<LogIn setCurrentUser={setCurrentUser} />
-							)
-						}
-					/>
-					<Route
-						path='sign-up'
-						element={currentUser ? <Navigate to='/dashboard' /> : <Signup />}
-					/>
-					<Route
-						path='dashboard'
-						element={
-							currentUser ? (
-								<>
-									<Outlet />
-								</>
-							) : (
-								<Navigate to='/log-in' />
-							)
-						}
-					>
-						<Route path='' element={<Dashboard currentUser={currentUser} />} />
-						<Route path='create' element={<PostForm />} />
-						<Route
-							path='update/:postid'
-							element={<PostForm editing={true} />}
-						/>
-						<Route
-							path='delete/:postid'
-							element={<PostDetails deleting={true} />}
-						/>
-					</Route>
-					<Route path='posts'>
-						<Route path='' element={<PostList />} />
-						<Route path=':postid' element={<Outlet />}>
-							<Route
-								path=''
-								element={
-									<>
-										<PostDetails />
-										<CommentList currentUser={currentUser} />
-									</>
-								}
-							/>
-							<Route
-								path='comments/:commentid'
-								element={<CommentDetails currentUser={currentUser} />}
-							/>
-						</Route>
-					</Route>
-					<Route path='authors' element={<AuthorList />} />
 				</Route>
-			</Routes>
-		</BrowserRouter>
+				<Route path='posts'>
+					<Route path='' element={<PostList posts={posts} />} />
+					<Route path=':postid' element={<Outlet />}>
+						<Route
+							path=''
+							element={
+								<>
+									<PostDetails posts={posts} />
+									<CommentList currentUser={currentUser} />
+								</>
+							}
+						/>
+						<Route
+							path='comments/:commentid'
+							element={<CommentDetails currentUser={currentUser} />}
+						/>
+					</Route>
+				</Route>
+				<Route path='authors' element={<AuthorList posts={posts} />} />
+			</Route>
+		</Routes>
 	);
 };
 
