8b83d8354762cb6beb46d18853d861f2b0c4f307
mrjackmaster <jacektrg+github@gmail.com>
2022-02-03 23:41:37 +0100

    Minor restructuring

--- client/src/components/utils/PostLinkWrapper.js
+++ client/src/components/utils/PostLinkWrapper.js
@@ -5,15 +5,17 @@ const PostLinkWrapper = ({ post }) => {
 		<Link to={`/posts/${post._id}`} className='post'>
 			<h2 className='post-title'>Title: {post.title}</h2>
 			<p className='post-text'>{post.text}</p>
-			<h3 className='post-author'>Author: {post.author.username}</h3>
-			<h3 className='post-created'>
-				Published:{' '}
-				{new Date(post.create_timestamp).toLocaleString('en-GB', {
-					year: 'numeric',
-					month: 'numeric',
-					day: 'numeric',
-				})}
-			</h3>
+			<div className='post-metadata'>
+				<h3 className='post-author'>Author: {post.author.username}</h3>
+				<h3 className='post-created'>
+					Published:{' '}
+					{new Date(post.create_timestamp).toLocaleString('en-GB', {
+						year: 'numeric',
+						month: 'numeric',
+						day: 'numeric',
+					})}
+				</h3>
+			</div>
 		</Link>
 	);
 };
