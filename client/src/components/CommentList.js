b0cbb8e560664adc76653f56b855120c0484f0db
mrjackmaster <jacektrg+github@gmail.com>
2022-02-04 17:31:52 +0100

    Moving most funtionalities to PostDetails

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,43 +1,18 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import CommentForm from './CommentForm';
-import CommentLinkWrapper from './utils/CommentLinkWrapper';
+import CommentWrapper from './utils/CommentWrapper';
 
-const CommentList = ({ currentUser }) => {
-	const params = useParams();
-
-	const [allComments, setAllComments] = useState();
-
-	useEffect(() => {
-		(async () => {
-			try {
-				const res = await fetch(`/api/posts/${params.postid}/comments`, {
-					method: 'GET',
-					mode: 'cors',
-					headers: { 'Content-type': 'application/json' },
-				});
-				const resJson = await res.json();
-				if (res.status !== 200) {
-					console.log(resJson);
-				} else {
-					setAllComments(resJson);
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, [params.postid]);
-
-	const commentsDisplay = allComments?.map((comment, index) => {
-		return <CommentLinkWrapper key={index} comment={comment} />;
+const CommentList = ({ currentUser, postComments, setPostComments }) => {
+	const commentsDisplay = postComments?.map((comment, index) => {
+		return (
+			<CommentWrapper
+				key={index}
+				currentUser={currentUser}
+				comment={comment}
+				setPostComments={setPostComments}
+			/>
+		);
 	});
 
-	return (
-		<>
-			<CommentForm currentUser={currentUser} setAllComments={setAllComments} />
-			<div className='comment-list'>{commentsDisplay}</div>
-		</>
-	);
+	return <div className='comment-list'>{commentsDisplay}</div>;
 };
 
 export default CommentList;
