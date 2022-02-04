53db0fc3bcdb84f01b4ad52a53053d5ef5b7b547
mrjackmaster <jacektrg+github@gmail.com>
2022-02-04 15:22:46 +0100

    Cleanup

--- client/src/components/utils/CommentLinkWrapper.js
+++ client/src/components/utils/CommentLinkWrapper.js
@@ -1,30 +1,26 @@
-import { Link } from 'react-router-dom';
-
 const CommentLinkWrapper = ({ comment }) => {
 	return (
-		<Link to={`comments/${comment._id}`}>
-			<div className='comment'>
-				<h3>{comment.author.username}</h3>
+		<div className='comment'>
+			<h3>{comment.author.username}</h3>
+			<h3>
+				{new Date(comment.create_timestamp).toLocaleString('en-GB', {
+					year: 'numeric',
+					month: 'numeric',
+					day: 'numeric',
+				})}
+			</h3>
+			{comment.update_timestamp ? (
 				<h3>
-					{new Date(comment.create_timestamp).toLocaleString('en-GB', {
+					Last edit:{' '}
+					{new Date(comment.update_timestamp).toLocaleString('en-GB', {
 						year: 'numeric',
 						month: 'numeric',
 						day: 'numeric',
 					})}
 				</h3>
-				{comment.update_timestamp ? (
-					<h3>
-						Last edit:{' '}
-						{new Date(comment.update_timestamp).toLocaleString('en-GB', {
-							year: 'numeric',
-							month: 'numeric',
-							day: 'numeric',
-						})}
-					</h3>
-				) : null}
-				<p>{comment.text}</p>
-			</div>
-		</Link>
+			) : null}
+			<p>{comment.text}</p>
+		</div>
 	);
 };
 
