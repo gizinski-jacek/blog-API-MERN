0e5103f699ea1c2096abee2317e638c5a6dfee5d
mrjackmaster <jacektrg+github@gmail.com>
2022-02-04 23:00:19 +0100

    Major cleanup. Moving most functionality to new component for editing comments

--- client/src/components/utils/CommentWrapper.js
+++ client/src/components/utils/CommentWrapper.js
@@ -1,123 +1,32 @@
-import { useState } from 'react';
-import { useParams } from 'react-router';
+import nl2br from 'react-nl2br';
 
-const CommentWrapper = ({ currentUser, comment, setPostComments }) => {
-	const params = useParams();
-
-	const [errors, setErrors] = useState();
-	const [editing, setEditing] = useState(false);
-	const [commentValue, setCommentValue] = useState(comment.text);
-	const [theComment, setTheComment] = useState(comment);
-
-	const handleUpdate = async (e) => {
-		e.preventDefault();
-		try {
-			const res = await fetch(
-				`/api/posts/${params.postid}/comments/${theComment._id}`,
-				{
-					method: 'PUT',
-					mode: 'cors',
-					credentials: 'include',
-					body: JSON.stringify({ commentValue }),
-					headers: { 'Content-type': 'application/json' },
-				}
-			);
-			const resJson = await res.json();
-			if (res.status !== 200) {
-				setErrors(resJson);
-			} else {
-				setEditing(false);
-				setCommentValue(resJson.text);
-				setTheComment(resJson);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const handleDelete = async (e) => {
-		e.preventDefault();
-		try {
-			const res = await fetch(
-				`/api/posts/${params.postid}/comments/${theComment._id}`,
-				{
-					method: 'DELETE',
-					mode: 'cors',
-					credentials: 'include',
-					headers: { 'Content-type': 'application/json' },
-				}
-			);
-			const resJson = await res.json();
-			if (res.status !== 200) {
-				setErrors(resJson);
-			} else {
-				setPostComments(resJson);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const errorsDisplay = errors?.map((error, index) => {
-		return (
-			<li key={index} className='error-msg'>
-				{error.msg}
-			</li>
-		);
-	});
-
+const CommentWrapper = ({ currentUser, comment, setEditingComment }) => {
 	return (
 		<div className='comment'>
-			{currentUser && currentUser._id === theComment.author._id ? (
+			{currentUser && currentUser._id === comment.author._id ? (
 				<div className='comment-edit-controls'>
 					<button
 						type='button'
+						className='button-s'
 						onClick={() => {
-							setEditing(!editing);
+							setEditingComment(comment);
 						}}
 					>
 						Edit
 					</button>
-					<button type='submit' onClick={handleDelete}>
-						Delete
-					</button>
 				</div>
 			) : null}
-			<h3 className='comment-author'>By: {theComment.author.username}</h3>
+			<h3 className='comment-author'>By: {comment.author.username}</h3>
 			<p className='comment-created'>
-				Posted: {new Date(theComment.create_timestamp).toLocaleString('en-GB')}
+				Posted: {new Date(comment.create_timestamp).toLocaleString('en-GB')}
 			</p>
-			{theComment.update_timestamp ? (
+			{comment.update_timestamp ? (
 				<p className='comment-updated'>
 					Last edited:{' '}
-					{new Date(theComment.update_timestamp).toLocaleString('en-GB')}
+					{new Date(comment.update_timestamp).toLocaleString('en-GB')}
 				</p>
 			) : null}
-			{editing ? (
-				<>
-					<form id='edit-comment-form' onSubmit={handleUpdate}>
-						<input
-							type='text'
-							id='comment'
-							name='comment'
-							minLength='2'
-							maxLength='64'
-							onChange={(e) => {
-								setCommentValue(e.target.value);
-							}}
-							value={commentValue}
-							placeholder='Comment'
-							required
-						/>
-						<button type='submit'>Update</button>
-					</form>
-					{errorsDisplay ? (
-						<ul className='error-list'>{errorsDisplay}</ul>
-					) : null}
-				</>
-			) : (
-				<p className='comment-text'>{theComment.text}</p>
-			)}
+			<p className='comment-text'>{nl2br(comment.text)}</p>
 		</div>
 	);
 };
