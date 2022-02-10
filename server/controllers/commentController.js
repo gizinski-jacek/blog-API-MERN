340c007196e0f27407b1f2dc8af2e6c03fa20198
mrjackmaster <jacektrg+github@gmail.com>
2022-02-10 21:59:02 +0100

    Updating timestamps. Returning errors in strings. Let frontend format them the way the want. Creating new comment on failed update by id. Updating update controller to reflect changes in frontend.

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -8,7 +8,7 @@ exports.get_all_post_comments = async (req, res, next) => {
 			return res.status(404).json('Invalid post Id');
 		}
 		const comment_list = await Comment.find({ post: req.params.postid })
-			.sort({ create_timestamp: 'desc' })
+			.sort({ createdAt: 'desc' })
 			.populate('author', 'username')
 			.exec();
 		res.status(200).json(comment_list);
@@ -18,7 +18,7 @@ exports.get_all_post_comments = async (req, res, next) => {
 };
 
 exports.create_comment = [
-	body('commentValue', 'Text field can not be empty')
+	body('text', 'Text field can not be empty')
 		.trim()
 		.isLength({ min: 4, max: 64 })
 		.escape(),
@@ -26,22 +26,19 @@ exports.create_comment = [
 		try {
 			const errors = validationResult(req);
 			const newComment = new Comment({
-				text: req.body.commentValue,
-				author: req.user._id,
+				text: req.body.text,
 				post: req.params.postid,
-				create_timestamp: new Date(),
+				author: req.user._id,
 			});
 			if (!errors.isEmpty()) {
 				return res.status(404).json(errors.array());
 			}
 			const comment = await newComment.save();
 			if (!comment) {
-				return res
-					.status(404)
-					.json([{ msg: 'Error saving comment, try again' }]);
+				return res.status(404).json('Error saving comment, try again');
 			}
 			const comment_list = await Comment.find({ post: req.params.postid })
-				.sort({ create_timestamp: 'desc' })
+				.sort({ createdAt: 'desc' })
 				.populate('author', 'username')
 				.exec();
 			res.status(200).json(comment_list);
@@ -52,7 +49,7 @@ exports.create_comment = [
 ];
 
 exports.update_comment = [
-	body('commentValue', 'Text field can not be empty')
+	body('text', 'Text field can not be empty')
 		.trim()
 		.isLength({ min: 2, max: 64 })
 		.escape(),
@@ -61,20 +58,12 @@ exports.update_comment = [
 			if (!mongoose.Types.ObjectId.isValid(req.params.commentid)) {
 				return res.status(404).json('Invalid comment Id');
 			}
-			const commentToUpdate = await Comment.findById(
-				req.params.commentid
-			).exec();
-			if (!commentToUpdate) {
-				return res.status(404).json('Comment not found, nothing to update');
-			}
 			const errors = validationResult(req);
 			const updatedComment = new Comment({
-				_id: commentToUpdate._id,
-				text: req.body.commentValue,
-				author: commentToUpdate.author,
-				post: commentToUpdate.post,
-				create_timestamp: commentToUpdate.create_timestamp,
-				update_timestamp: new Date(),
+				_id: req.params.commentid,
+				text: req.body.text,
+				post: req.body.post,
+				author: req.user._id,
 			});
 			if (!errors.isEmpty()) {
 				return res.status(404).json(errors.array());
@@ -82,15 +71,17 @@ exports.update_comment = [
 			const comment = await Comment.findByIdAndUpdate(
 				req.params.commentid,
 				updatedComment,
-				{ returnDocument: 'after' }
-			)
-				.populate('author', 'username')
-				.exec();
+				{ upsert: true }
+			).exec();
 			if (!comment) {
-				return res.status(404).json('Comment not found, nothing to update');
+				return res
+					.status(404)
+					.json(
+						'Comment not found, nothing to update. Creating comment instead'
+					);
 			}
 			const comment_list = await Comment.find({ post: req.params.postid })
-				.sort({ create_timestamp: 'desc' })
+				.sort({ createdAt: 'desc' })
 				.populate('author', 'username')
 				.exec();
 			res.status(200).json(comment_list);
@@ -112,7 +103,7 @@ exports.delete_comment = async (req, res, next) => {
 			return res.status(404).json('Comment not found, nothing to delete');
 		}
 		const comment_list = await Comment.find({ post: req.params.postid })
-			.sort({ create_timestamp: 'desc' })
+			.sort({ createdAt: 'desc' })
 			.populate('author', 'username')
 			.exec();
 		res.status(200).json(comment_list);
