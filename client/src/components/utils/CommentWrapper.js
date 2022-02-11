f5ad7b8af32e087d166dd716be61d9130e30a892
mrjackmaster <jacektrg+github@gmail.com>
2022-02-11 15:12:58 +0100

    Wrapping comment data

--- client/src/components/utils/CommentWrapper.js
+++ client/src/components/utils/CommentWrapper.js
@@ -16,15 +16,17 @@ const CommentWrapper = ({ currentUser, comment, setEditingComment }) => {
 					</button>
 				</div>
 			) : null}
-			<h3 className='comment-author'>By: {comment.author.username}</h3>
-			<h5 className='comment-created'>
-				Posted: {new Date(comment.createdAt).toLocaleString('en-GB')}
-			</h5>
-			{comment.updatedAt ? (
-				<h5 className='comment-updated'>
-					Last edit: {new Date(comment.updatedAt).toLocaleString('en-GB')}
+			<div className='comment-metadata'>
+				<h3 className='comment-author'>By: {comment.author.username}</h3>
+				<h5 className='comment-created'>
+					Posted: {new Date(comment.createdAt).toLocaleString('en-GB')}
 				</h5>
-			) : null}
+				{comment.updatedAt ? (
+					<h5 className='comment-updated'>
+						Last edit: {new Date(comment.updatedAt).toLocaleString('en-GB')}
+					</h5>
+				) : null}
+			</div>
 			<p className='comment-text'>{nl2br(comment.text)}</p>
 		</div>
 	);
