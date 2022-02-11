a16105314ed8a373ec4af0d59ab3ecda91cda303
mrjackmaster <jacektrg+github@gmail.com>
2022-02-11 18:49:50 +0100

    Cleanup

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* Redirect to API. */
-router.get('/', function (req, res, next) {
-	res.redirect('/api');
-});
-
-module.exports = router;
