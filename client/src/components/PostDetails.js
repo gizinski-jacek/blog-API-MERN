72dd88b90cc8096fd08ba1e38b0527785c551fda
mrjackmaster <jacektrg+github@gmail.com>
2022-02-14 18:01:00 +0100

    Separating delete functionality. If post is not published the details page will be shown only to the author of the post, otherwise not found message will be displayed.

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
-import { Link } from 'react-router-dom';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 import EditCommentForm from './EditCommentForm';
 import LoadingIcon from './utils/LoadingIcon';
 import nl2br from 'react-nl2br';
 
-const PostDetails = ({ currentUser, deleting }) => {
+const PostDetails = ({ currentUser }) => {
 	const navigate = useNavigate();
 	const params = useParams();
 
@@ -51,72 +50,42 @@ const PostDetails = ({ currentUser, deleting }) => {
 		})();
 	}, [params.postid, navigate]);
 
-	const handleDelete = async (e) => {
-		try {
-			const res = await fetch(`/api/posts/${params.postid}`, {
-				method: 'DELETE',
-				mode: 'cors',
-				credentials: 'include',
-				headers: { 'Content-type': 'application/json' },
-			});
-			const resJson = await res.json();
-			if (res.status !== 200) {
-				console.log(resJson);
-			}
-			navigate('/dashboard');
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
 	return (
 		<>
 			{loading ? (
 				<LoadingIcon />
-			) : (
+			) : post.published ||
+			  (!post.published && currentUser?._id === post.author._id) ? (
 				<div className='post-details'>
-					{deleting ? (
-						<div className='post-delete-controls'>
-							<h1>Delete this post?</h1>
-							<button type='submit' className='button-l' onClick={handleDelete}>
-								Delete
-							</button>
-							<Link to='/dashboard' className='button-l'>
-								Cancel
-							</Link>
-						</div>
-					) : null}
-					{post ? (
-						<article className='post-full'>
-							<div className='post-info'>
-								<div className='post-metadata'>
-									<div className='left'>
-										<h2 className='post-author'>
-											Author: {post.author.username}
-										</h2>
-									</div>
-									<div className='right'>
-										<h5 className='post-created'>
-											Published:{' '}
-											{new Date(post.createdAt).toLocaleString('en-GB')}
-										</h5>
-										{post.updatedAt ? (
-											<h5 className='post-updated'>
-												Updated:{' '}
-												{new Date(post.updatedAt).toLocaleString('en-GB')}
-											</h5>
-										) : null}
-									</div>
+					<article className='post-full'>
+						<div className='post-info'>
+							<div className='post-metadata'>
+								<div className='left'>
+									<h2 className='post-author'>
+										Author: {post.author.username}
+									</h2>
 								</div>
-								<h2 className='post-title'>Title: {post.title}</h2>
+								<div className='right'>
+									<h5 className='post-created'>
+										Published:{' '}
+										{new Date(post.createdAt).toLocaleString('en-GB')}
+									</h5>
+									{post.updatedAt ? (
+										<h5 className='post-updated'>
+											Updated:{' '}
+											{new Date(post.updatedAt).toLocaleString('en-GB')}
+										</h5>
+									) : null}
+								</div>
 							</div>
-							<div className='post-content'>
-								<p className='post-text'>{nl2br(post.text)}</p>
-							</div>
-						</article>
-					) : null}
-					{deleting ? null : (
-						<div className='comment-section'>
+							<h2 className='post-title'>Title: {post.title}</h2>
+						</div>
+						<div className='post-content'>
+							<p className='post-text'>{nl2br(post.text)}</p>
+						</div>
+					</article>
+					<div className='comment-section'>
+						{post.published ? (
 							<div className='comment-controls'>
 								<CommentForm
 									currentUser={currentUser}
@@ -130,13 +99,30 @@ const PostDetails = ({ currentUser, deleting }) => {
 									/>
 								) : null}
 							</div>
-							<CommentList
-								currentUser={currentUser}
-								postComments={postComments}
-								setEditingComment={setEditingComment}
-							/>
-						</div>
-					)}
+						) : null}
+						<CommentList
+							currentUser={currentUser}
+							postComments={postComments}
+							setEditingComment={setEditingComment}
+						/>
+					</div>
+				</div>
+			) : (
+				<div className='not-found'>
+					<div className='not-found-info'>
+						<h3>Post not found.</h3>
+						<p>
+							The post you tried to access was not found. It was either deleted
+							or unpublished by the author.
+						</p>
+						<button
+							type='button'
+							className='button-m'
+							onClick={() => navigate(-1)}
+						>
+							Go Back
+						</button>
+					</div>
 				</div>
 			)}
 		</>
